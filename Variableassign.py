4a55970154260687f1944196f2b1909584c34b33
ekemmstar <103825682+ekemmstar@users.noreply.github.com>
2022-06-06 22:06:55 +0300

    initial commit

---
+++ Variableassign.py
@@ -0,0 +1,44 @@
+# Question one:
+# Create a variable named car_name and assign the string value Volvo to it.
+car_name = "Volvo"
+
+# Question two: 
+#Create a variable named x and assign the integer value 50 to it, and print it to the console
+x = 50
+print(x)
+
+# Question three: 
+#Display the sum of `5 + 10`, using two variables: `x` and `y`. To solve this,
+   # 1. Create a variable `x` and assign the integer value 5 to it
+    #2. Create a variable `y` and assign the integer value 10 to it
+    #3. Print out the sum of `x` and `y`
+x = 5
+y = 10
+print(x+y)
+
+#Question four. 
+#Display the sum of `43 + 98`, using three variables: `x` ,`y` and `z`. To solve this,
+    #1. Create a variable `x` and assign the integer value 43 to it
+    #2. Create a variable `y` and assign the integer value 98 to it
+    #3. Create a variable `z` and assign the sum of x and y to it
+    #4. Print out the variable `z`
+
+x = 43
+y = 98
+z = x + y
+print(z)
+
+#Question Five: 
+#Display your full name by going through the steps below
+    #1. Create a variable `first_name` and assign your first name to it
+    #2. Create a variable `last_name` and assign your last name to it
+    #3. Create a variable `full_name` and assign `first_name + last_name` to it
+    #4. Print out the variable `full_name`
+first_name = "Esther"
+last_name =  "Michael"
+full_name = first_name  +  last_name
+print("My Fullname is: " + full_name)
+
+
+
+
